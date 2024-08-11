ce55351506e1666abe2b5e8e42405da990853653
Aman Srivastava <39951097+Amanumang@users.noreply.github.com>
2024-08-11 15:57:52 +0530

    Add files via upload

---
+++ Baisc_Program/Type_Casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a = 'a';
+    cout << a << endl;
+    
+    // output - 97 (convert into ASCII code)
+
+    char ch = 98;
+    cout << ch << endl;
+    // output - b
+
+}
